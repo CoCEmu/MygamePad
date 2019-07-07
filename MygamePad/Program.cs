73d8b21dae9305fc8e1e7bec37bbd684e8675727
mamad.hosein github <mamad.hosein@gmail.com>
2019-07-07 17:02:07 +0430

    first

---
+++ MygamePad/Program.cs
@@ -0,0 +1,74 @@
+﻿using SlimDX.DirectInput;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MygamePad
+{
+    class Program
+    {
+        static void Main(string[]args)
+        {
+            // Initialize DirectInput
+            var directInput = new DirectInput();
+
+            // Find a Joystick Guid
+            var joystickGuid = Guid.Empty;
+            /* var mioo = directInput.GetDevices(DeviceType.Gamepad,
+                         DeviceEnumerationFlags.AttachedOnly);*/
+            foreach (var deviceInstance in directInput.GetDevices(DeviceType.Gamepad,
+                        DeviceEnumerationFlags.AllDevices))
+                joystickGuid = deviceInstance.InstanceGuid;
+
+            // If Gamepad not found, look for a Joystick
+            if (joystickGuid == Guid.Empty)
+                foreach (var deviceInstance in directInput.GetDevices(DeviceType.Joystick,
+                        DeviceEnumerationFlags.AllDevices))
+                    joystickGuid = deviceInstance.InstanceGuid;
+
+            // If Joystick not found, throws an error
+            if (joystickGuid == Guid.Empty)
+            {
+                Console.WriteLine("No joystick/Gamepad found.");
+                Console.ReadKey();
+                Environment.Exit(1);
+            }
+
+            // Instantiate the joystick
+            var joystick = new Joystick(directInput, joystickGuid);
+
+            Console.WriteLine("Found Joystick/Gamepad with GUID: {0}", joystickGuid);
+
+            // Query all suported ForceFeedback effects
+            var allEffects = joystick.GetEffects();
+            foreach (var effectInfo in allEffects)
+                Console.WriteLine("Effect available {0}", effectInfo.Name);
+
+            // Set BufferSize in order to use buffered data.
+            joystick.Properties.BufferSize = 2;
+
+            // Acquire the joystick
+            joystick.Acquire();
+
+            // Poll events from joystick
+            while (true)
+            {
+                joystick.Poll();
+                var datas = joystick.GetBufferedData();
+                foreach (var state in datas)
+                {
+                    //   Console.WriteLine("x:" + ((state.X / 100)-327.5).ToString());
+                    // Console.WriteLine("y:" + ((state.Y / 100) - 327.5).ToString());
+                    //  Console.WriteLine("z:" + state.Z.ToString());
+                    for (int i = 0; i < 128; i++)
+                    {
+                        if (state.GetButtons()[i])
+                            Console.WriteLine("Button:" + i.ToString());
+                    }
+                }
+
+            }
+        }
+    }
+}
